720b69e1c7245fded8c651f8cbc6a70c08d2eae0
Maco <Maco@LAPTOP-O1JLL8OB>
2020-04-09 18:58:10 +0200

    Esercizio 1 completo

---
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/AlienDictionary.java
@@ -0,0 +1,27 @@
+package it.polito.tdp.alien;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlienDictionary {
+	
+	private List<Word>dizionario=new ArrayList<>();
+	
+	public void add(String alienWord, String translation) {
+		Word w=new Word(alienWord, translation);
+		if(dizionario.contains(w)) {
+			dizionario.get(dizionario.indexOf(w)).setTranslation(translation);
+			return;
+		}
+		dizionario.add(w);
+	}
+	
+	public String translateWord(String alienWord) {
+		Word w=new Word(alienWord);
+		if(dizionario.contains(w)) {
+			return dizionario.get(dizionario.indexOf(w)).getTranslation();
+		}
+		return null;
+	}
+
+}
--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -2,6 +2,8 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +11,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private AlienDictionary dizionario=new AlienDictionary();
 
     @FXML
     private ResourceBundle resources;
@@ -30,11 +34,54 @@ public class FXMLController {
 
     @FXML
     void doReset(ActionEvent event) {
+    	txtResult.clear();
+    	txtInsert.clear();
 
     }
 
     @FXML
     void doTranslate(ActionEvent event) {
+    	
+    	String riga=txtInsert.getText().toLowerCase();
+    	
+    	if(riga.length()==0) {
+    		txtResult.setText("Inserire due parole per aggiungerle al dizionario\no una parola per avere una traduzione\n");
+    		return;
+    	}
+    	
+    	StringTokenizer st= new StringTokenizer(riga, " ");
+    	
+    	String alienWord=st.nextToken();
+    	
+    	//inserire parola e traduzione
+    	if(st.hasMoreTokens()) {
+    		String translation=st.nextToken();
+    		
+    		if(!alienWord.matches("[a-zA-Z]*") || !translation.matches("[a-zA-Z]*")) {
+    			txtResult.setText("Inserire solo caratteri alfabetici\n");
+    			return;
+    		}
+    		
+    		dizionario.add(alienWord, translation);
+    		txtResult.setText("La parola "+alienWord+" con traduzione "+translation+" è stata inserita nel dizionario con successo!\n");
+    	}
+    	else {
+    		//inserire la parola da tradurre
+    		if(!alienWord.matches("[a-zA-Z]*")) {
+    			txtResult.setText("Inserire solo caratteri alfabetici\n");
+    			return;
+    		}
+    		String translation=dizionario.translateWord(alienWord);
+    		if(translation!=null) {
+    			txtResult.setText(translation);
+    		}
+    		else {
+    			txtResult.setText("La parola non è presente nel dizionario\n");
+    		}
+    		
+    	}
+    	
+    	
 
     }
 
